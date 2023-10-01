7fdac9bb81c112ea2014136a0859386bd9c088ae
Abubakar9894 <abubakarsaddique786.tsk@gmail.com>
2023-10-01 22:07:26 +0500

    sixth commit

--- src/components/TodoWrapperLocalStorage.js
+++ src/components/TodoWrapperLocalStorage.js
@@ -4,3 +4,20 @@ import { v4 as uuidv4 } from 'uuid';
 import { Todo } from './Todo';
 import { EditTodoForm } from './EditTodoForm';
 uuidv4();
+
+
+export const TodoWrapperLocalStorage = () => {
+    const [todos, setTodos] = useState([])
+
+    useEffect(() => {
+        const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+        setTodos(savedTodos);
+    }, []);
+
+    const addTodo = todo => {
+        const newTodos = [...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}];
+        setTodos(newTodos);
+        localStorage.setItem('todos', JSON.stringify(newTodos)
+        );
+    }
+}
