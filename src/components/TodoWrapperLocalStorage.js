d9fc8551f70f9c0cd3ae96fb81f80b7582983bce
Abubakar9894 <abubakarsaddique786.tsk@gmail.com>
2023-10-01 22:10:14 +0500

    seventh commit

--- src/components/TodoWrapperLocalStorage.js
+++ src/components/TodoWrapperLocalStorage.js
@@ -20,4 +20,40 @@ export const TodoWrapperLocalStorage = () => {
         localStorage.setItem('todos', JSON.stringify(newTodos)
         );
     }
-}
+    const toggleComplete = id => {
+        const newTodos = todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo);
+        setTodos(newTodos);
+        localStorage.setItem('todos', JSON.stringify(newTodos));
+    }
+
+    const deleteTodo = id => {
+        const newTodos = todos.filter(todo => todo.id !== id);
+        setTodos(newTodos);
+        localStorage.setItem('todos', JSON.stringify(newTodos));
+    }
+
+    const editTodo = id => {
+        setTodos(todos.map(todo => todo.id === id ? {...todo, isEditing: !todo.isEditing} : todo))
+    }
+
+    const editTask = (task, id) => {
+        const newTodos = todos.map(todo => todo.id === id ? {...todo, task, isEditing: !todo.isEditing} : todo);
+        setTodos(newTodos);
+        localStorage.setItem('todos', JSON.stringify(newTodos));
+    }
+  return (
+    <div className='TodoWrapper'>
+        <h1>Get Things Done!</h1>
+        <TodoForm addTodo={addTodo} />
+        {todos.map((todo, index) => (
+            todo.isEditing ? (
+                <EditTodoForm editTodo={editTask} task={todo} />
+            ) : (
+                <Todo task={todo} key={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo} editTodo={editTodo} />
+            )
+            
+        ))}
+         
+    </div>
+  )
+};
